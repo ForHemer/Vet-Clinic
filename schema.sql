8087a7bc7b8dbf926d41c2ce95172c70cbf576bf
ForHemer <foreste.hemerson@gmail.com>
2022-04-27 13:36:25 -0400

    Create vet_clinic database and animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+CREATE DATABASE vet_clinic;
+CREATE TABLE animals(
+    id integer primary key,
+    name varchar(100) not null,
+    date_of_birth date not null,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg real
 );
