70e43bd8b05d944bee0522b2048e49ed91a26bd5
ForHemer <foreste.hemerson@gmail.com>
2022-04-27 15:39:51 -0400

    Insert data into animals table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals(
-    id integer primary key,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name varchar(100) not null,
     date_of_birth date not null,
     escape_attempts integer,
