efc57d114a836a205437604c082ca6ad904b5f1b
ForHemer <foreste.hemerson@gmail.com>
2022-04-29 19:45:07 -0400

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -7,3 +7,11 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '2020-02-08', 0, FALSE, -11), 
 ('Plantmon', '2021-11-15', 2, TRUE, -5.7), ('Squirtle', '1993-04-02', 3, FALSE, -12.13), ('Angemon', '2005-06-12', 1, TRUE, -45), 
 ('Boarmon', '2005-06-07', 7, TRUE, 20.4), ('Blossom', '1998-10-13', 3, TRUE, 17), ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+/* Insert data into owners table*/
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
